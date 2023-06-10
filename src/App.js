9538b36dd7b96e9498bbbb7c57ebef61e7538df4
samkim <milk3531@hotmail.com>
2023-06-09 20:55:47 -0700

    init packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState,useEffect} from 'react';
+import axios from "axios";
+import "./App.css";
+
+
 
 function App() {
+  
+
+  const [product, setProduct] = useState([]);
+  
+  const [input, setInput] = useState('');
+
+ 
+function DataLoaded() {
+  let productList = [];
+  for (const [key, value] of Object.entries(product)) {
+    productList.push(<div key={key}>
+      <div className="item-image"> <img src ={value.Image} alt = {value.Name} ></img> </div>
+       <div className="Item-box"> <a href = {value.Link} target="_blank"><h1>Purchase Link</h1></a>
+                                    <h1>{value.Name}  </h1>
+                                    <h1>{value.Price} </h1> 
+                                    <h1>{value.Rating}</h1> </div>
+                                    </div>)
+  }
+  return productList;
+}
+
+function DataLoading()  {
+  return <h1>  </h1>
+}
+
+function fetchData() {
+  if (product.length == 0) {
+    return <DataLoading/>
+  } else return <DataLoaded/>
+}
+
+
+
+
+
+function getData() {
+  axios.get('http://localhost:3001/', {
+    params: {
+      data: input
+    },
+    crossdomain: true // pass the crossdomain property as part of the second parameter
+  })
+  .then(res => {
+    console.log(res);
+    setProduct(res.data);
+  })
+  .then(() => {
+    fetchData();
+  });
+}
+
+
+
+
+
+  useEffect(() => {
+
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="title"> Amazon WebScraper (Canada)</div>
+      <div className="search-button"> 
+      <input className="input" type="text" onInput={e => setInput(e.target.value)}/>
+      <button className="button" onClick={() => getData()}>Search</button>
+                                                        </div>
+
+         <div className="container">  <div className="head">
+         {/* <h1>Product</h1>
+         <h1>Rating</h1>
+         <h1>Price</h1>
+         <h1>Link</h1> */}
+         </div>
+         {fetchData()}</div>
+  </div>
+      
   );
 }
 
